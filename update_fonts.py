6e0c822775adea43f514e2a9faf22a541de2516f
Dogga Raveendhra <raveendogga@gmail.com>
2026-06-11 22:17:54 +0530

    Redesign: Match Google Sites Maroon/White style

--- update_fonts.py
+++ update_fonts.py
@@ -5,7 +5,7 @@ files = glob.glob('*.html')
 content_to_find = '<link rel="stylesheet" href="styles.css">'
 content_to_replace = """<link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;600;800;900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Merriweather:wght@300;400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="styles.css">"""
 
 for f in files:
